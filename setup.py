fb601f38e5d50e0f6e79e46e11199f11551c917b
caesuric <illusorycaesura@gmail.com>
2021-10-19 21:18:53 -0400

    set up things for initial release

--- setup.py
+++ setup.py
@@ -1,19 +1,43 @@
 from setuptools import setup
 setup(
-    name='homebrewery-to-enki',
+    name='homebrewery-to-libris',
     version='1.0.0',
-    description='Converter between homebrewery and enki markdown formats.',
-    url='https://github.com/lazy-scrivener-games/homebrewery-to-enki',
+    description='Converter between homebrewery and libris markdown formats.',
+    url='https://github.com/lazy-scrivener-games/homebrewery-to-libris',
+    download_url='FILL_THIS_OUT_______________________________________________',
     author='Chris Muller',
     author_email='chris@lazyscrivenergames.com',
+    keywords=[
+        'utility',
+        'pdf',
+        'html',
+        'markdown',
+        'conversion',
+        'book',
+        'roleplaying',
+        'game',
+        'homebrewery',
+        'libris'
+    ],
     license='MIT',
     packages=[
-        'homebrewery-to-enki'
+        'homebrewery-to-libris'
     ],
     scripts=[
-        'scripts/homebrewery-to-enki'
+        'scripts/homebrewery-to-libris'
     ],
     install_requires=[
         'markdown2 == 2.4.1'
+    ],
+        classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Text Processing :: Markup :: Markdown',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ]
 )
